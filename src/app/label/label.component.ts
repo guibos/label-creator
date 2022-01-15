7cfd9a4aa99ed093beeec06802748c5a28bbcdbf
guibos <22233599+guibos@users.noreply.github.com>
2022-01-15 12:20:13 +0100

    WIP

---
+++ src/app/label/label.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-label',
+  templateUrl: './label.component.html',
+  styleUrls: ['./label.component.css']
+})
+export class LabelComponent {
+  public items: string[] = ['Label1', 'Label2'];
+  
+
+}
